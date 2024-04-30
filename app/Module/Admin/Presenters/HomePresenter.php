0fa29addf8e1027025ba76dc7a3ae27808fdd532
JancaF <filip.janca@student.ossp.cz>
2024-04-30 12:05:53 +0200

    commit

--- app/Module/Admin/Presenters/HomePresenter.php
+++ app/Module/Admin/Presenters/HomePresenter.php
@@ -13,18 +13,10 @@ final class HomePresenter extends Nette\Application\UI\Presenter
 	) {
 	}
 	
-	public function renderDefault(int $page = 1): void { 
+	public function renderDefault(int $postId): void { 
 		$this->template->posts = $this->facade
 		   ->getPublicArticles()
 		   ->limit(5);
-		   $articlesCount = $this->facade->getPublishedArticlesCount();
-		   $paginator = new Nette\Utils\Paginator;
-		   $paginator->setItemCount($articlesCount); 
-		   $paginator->setItemsPerPage(10); 
-		   $paginator->setPage($page);
-   
-		   $articles = $this->facade->getPublicArticles($paginator->getLength(), $paginator->getOffset());
-		   $this->template->paginator = $paginator;
 	}
 	public function actionShow(int $postId) {
 	    $this->flashMessage('Nemáš právo vidět archived, kámo !');
